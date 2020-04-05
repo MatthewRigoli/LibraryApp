ddf8600a9fce539c0f6f3971be8afe1ff10c1113
McKinnin <lloydmckinnin@gmail.com>
2020-04-04 21:40:57 -0600

    pushing before restart

--- LibraryApp/LibraryApp/Views/MainPage.xaml.cs
+++ LibraryApp/LibraryApp/Views/MainPage.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using Plugin.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
 
 namespace LibraryApp.Views
 {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PhoneCall_Clicked(object sender, EventArgs e)
+        {
+            var phoneCall = CrossMessaging.Current.PhoneDialer;
+            if (phoneCall.CanMakePhoneCall)
+            {
+                phoneCall.MakePhoneCall("+4354623240", "Mckinnin!");
+            }
         }
     }
 }
