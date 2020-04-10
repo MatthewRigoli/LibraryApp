32d08dffbfc59d5bebc778a1034d8930452ba5a5
McKinnin <lloydmckinnin@gmail.com>
2020-04-10 12:29:52 -0600

    Got email to work. Currently just sends to mckinnins email

--- LibraryApp/LibraryApp/Views/MainPage.xaml.cs
+++ LibraryApp/LibraryApp/Views/MainPage.xaml.cs
@@ -27,5 +27,28 @@ namespace LibraryApp.Views
                 phoneCall.MakePhoneCall("+4354623240", "Mckinnin!");
             }
         }
+
+        private void Email_Clicked(object sender, EventArgs e)
+        {
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                // Send simple e-mail to single receiver without attachments, bcc, cc etc.
+                emailMessenger.SendEmail("lloydmckinnin@gmail.com",
+                                    "Xamarin Messaging Plugin",
+                                    "Well hello there from Xam.Messaging.Plugin");
+
+                // Alternatively use EmailBuilder fluent interface to construct more complex e-mail with multiple recipients, bcc, attachments etc.
+                var email = new EmailMessageBuilder()
+                  .To("lloydmckinnin@gmail.com")
+                  .Cc("plugins@xamarin.com")
+                  .Bcc(new[] { "plugins@xamarin.com", "plugins@xamarin.com" })
+                  .Subject("Xamarin Messaging Plugin")
+                  .Body("Well hello there from Xam.Messaging.Plugin")
+                  .Build();
+
+                emailMessenger.SendEmail(email);
+            }
+        }
     }
 }
