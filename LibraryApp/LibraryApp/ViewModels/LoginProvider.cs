464fd2751dbe93b3b10bc60c53c1d98826bde72f
Matt Rigoli <Saveferris2000@gmail.com>
2020-04-17 20:10:49 -0600

    Worked through the tutorial, got stuck on the Windows. section of my view model.

---
+++ LibraryApp/LibraryApp/ViewModels/LoginProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using LibraryApp.ViewModels;
+
+//Here is the tutorial I am using:
+//https://developer.okta.com/blog/2019/06/11/build-login-in-xamarin-with-xamarin-forms
+
+[assembly: Xamarin.Forms.Dependency(typeof(LoginProvider))]
+namespace LibraryApp.ViewModels
+{
+    class LoginProvider : ILoginProvider
+    {
+        public async Task<string> LoginAsync()
+        {
+            var clientId = "{yourClientId}";
+            var url = "https://{yourOktaDomain}/oauth2/default/v1/authorize";
+            var state = GetBase64UrlData(32);
+            var nonce = GetBase64UrlData(12);
+            var codeVerifier = GetBase64UrlData(32);
+            var tmp = CryptographicBuffer.ConvertStringToBinary(codeVerifier, BinaryStringEncoding.Utf8);
+            var codeVerifierBuffer = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Sha256).HashData(tmp);
+            var codeChallenge = GetBase64UrlData(null, codeVerifierBuffer);
+
+            var absoluteUri = WebAuthenticationBroker.GetCurrentApplicationCallbackUri().AbsoluteUri;
+            var callback = Uri.EscapeDataString(absoluteUri);
+
+            var authorizationRequest = string.Format(
+                "{0}?response_type=id_token&scope=openid profile&redirect_uri={1}&client_id={2}&state={3}&code_challenge={4}&code_challenge_method=S256&nonce={5}",
+                url, callback, clientId, state, codeChallenge, nonce);
+
+            var result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None,
+                new Uri(authorizationRequest), new Uri(absoluteUri));
+            if (result.ResponseStatus != WebAuthenticationStatus.Success) return null;
+            var queryParams = result.ResponseData.Split("#?&".ToCharArray());
+            var idToken = queryParams.FirstOrDefault(p => p.StartsWith("id_token"));
+            return idToken?.Substring(9);
+        }
+
+        public static string GetBase64UrlData(uint? length = null, IBuffer buffer = null)
+        {
+            if (length != null) buffer = CryptographicBuffer.GenerateRandom(length.Value);
+            return CryptographicBuffer.EncodeToBase64String(buffer)
+                .Replace("+", "-")
+                .Replace("/", "_")
+                .Replace("=", "");
+        }
+    }
+}
---
+++ LibraryApp/LibraryApp/Views/LoginView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LibraryApp.ViewModels;
+
+namespace LibraryApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginView : ContentView
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        public event EventHandler<string> LoginChanged;
+
+        private async void LoginClicked(object sender, EventArgs e)
+        {
+            var loginProvider = DependencyService.Get<ILoginProvider>();
+            var idToken = await loginProvider.LoginAsync();
+
+            string userName = null;
+            if (idToken != null)
+            {
+                var jwtHandler = new JwtSecurityTokenHandler();
+                var token = jwtHandler.ReadJwtToken(idToken);
+                userName = token.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value;
+            }
+
+            if (LoginChanged != null) LoginChanged(this, userName);
+
+            if (userName == null)
+            {
+                ErrorLabel.Text = "Login failed.";
+                return;
+            }
+
+            LoginPanel.IsVisible = false;
+            LogoutPanel.IsVisible = true;
+            ErrorLabel.Text = "";
+            LoggedInLabel.Text = "You are logged in as " + userName;
+        }
+    }
+}
