2ff1a13d5b74eae0eb80ecbaabd26d78a886c36e
McKinnin <lloydmckinnin@gmail.com>
2020-04-24 15:23:10 -0600

    Started addind a database

---
+++ LibraryApp/LibraryApp/Database/ILocalDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Database
+{
+    public interface ILocalDataService
+    {
+        void Inititalize();
+        void AddBook(Book book);
+
+        
+    }
+}
---
+++ LibraryApp/LibraryApp/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryApp.Model
+{
+    [Table("Book")]
+    public class Book 
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [Unique]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Summary { get; set; }
+
+    }
+}
